5ee4384e670785b1c6a522285b6a43b8e2ab0b44
Rushil Basappa <rushil.oucse37@gmail.com>
2023-10-16 01:19:02 -0700

    feat: add webpack support
    * add webpack for dev and build
    
    8685zwa26

---
+++ webpack/webpack.common.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const paths = require("./paths");
+
+module.exports = {
+  entry: "./src/main.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hot Module Replacement",
+      filename: "index.html",
+      template: "./index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: paths.dist,
+    clean: true,
+  },
+};
