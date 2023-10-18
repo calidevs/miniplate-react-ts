309b5bd13e1c8c1090ff3c3eaacd0b0ca94054ba
Rushil Basappa <rushil.oucse37@gmail.com>
2023-10-18 12:38:46 -0700

    refactor: minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { Button } from "Components";
+import { Button } from "Components/Button";
 
 function App() {
   const [count, setCount] = useState(0);
