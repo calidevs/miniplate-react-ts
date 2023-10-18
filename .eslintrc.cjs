8d9f23fecfaaf6a2177b799d561a03e463bd76e9
abhigattineni <abhishekgattineni@gmail.com>
2023-10-18 18:13:57 -0400

    changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,15 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/prettier",
+    "prettier/react",
+    "prettier/@typescript-eslint",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "prettier"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
